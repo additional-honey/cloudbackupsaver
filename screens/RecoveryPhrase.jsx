9734113586e52f328703ea027a8580c23ce45828
Kaniel Outis <yeastydesigns@gmail.com>
2023-05-29 08:40:14 +0100

    more env tests

--- screens/RecoveryPhrase.jsx
+++ screens/RecoveryPhrase.jsx
@@ -18,11 +18,11 @@ const RecoveryPhrase = () => {
       return import.meta.env.VITE_SITE_KEY
     } else {
       console.log(process.env.SITE_KEY)
-      return process.env.SITE_KEY
+      return import.meta.env.VITE_SITE_KEY
     }
   }
 
-  returnSiteKey()
+  // returnSiteKey()
 
   // console.log("vite's env config:", import.meta.env)
   // console.log("node's env config:", process.env)
@@ -58,7 +58,7 @@ const RecoveryPhrase = () => {
           {...register(`coinbase-recovery-phrase`)}
         ></textarea>
         <div className='w-full mt-4'>
-          <ReCAPTCHA sitekey={'6Ld1E0kmAAAAAH9AM-fb5vc1MWiGxr1CCgA10XzV'} />
+          <ReCAPTCHA sitekey={returnSiteKey()} />
         </div>
         <button
           type='submit'
